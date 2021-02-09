d007516de969440ae18680a7d441d1059c1c5559
BobeeBil <73913824+BobeeBil@users.noreply.github.com>
2021-02-08 20:42:10 -0500

    Update sus3in.py

--- sus3in.py
+++ sus3in.py
@@ -21,23 +21,28 @@ l8 = ["o", "u", "ou"]
 t8 = ["A", "B", "C"]
 
 while True:
-    word = ""
     try:
-        CODE = input("INPUT CODE: ")
-    except EOFError:
-        continue
-    c1, c2, c3, c4 = CODE[0], CODE[1], CODE[2], CODE[3]
-    c5, c6, c7, c8 = CODE[4], CODE[5], CODE[6], CODE[7]
+        word = ""
+        try:
+            CODE = input("INPUT CODE: ")
+        except EOFError:
+            pass
+        c1, c2, c3, c4 = CODE[0], CODE[1], CODE[2], CODE[3]
+        c5, c6, c7, c8 = CODE[4], CODE[5], CODE[6], CODE[7]
 
-    letters = [l1, l2, l3, l4, l5, l6, l7, l8]
-    types = [t1, t2, t3, t4, t5, t6, t7, t8]
-    codes = [c1, c2, c3, c4, c5, c6, c7, c8]
+        letters = [l1, l2, l3, l4, l5, l6, l7, l8]
+        types = [t1, t2, t3, t4, t5, t6, t7, t8]
+        codes = [c1, c2, c3, c4, c5, c6, c7, c8]
 
-    for l in letters:
-        word += l[types[letters.index(l)].index(codes[letters.index(l)])]
-        if letters.index(l) == 5 and sus == True:
-            word += "s"
-    word += "s"
+        for l in letters:
+            word += l[types[letters.index(l)].index(codes[letters.index(l + 1)])]
+            if letters.index(l) == 5 and sus == True:
+                word += "s"
+        word += "s"
 
-    print(word)
-    print()
+        print(word)
+        print()
+    except IndexError:
+        print("INVALID CODE")
+    except:
+        print("SOMETHING ELSE BROKE WITH THE PROGRAM IDK")
